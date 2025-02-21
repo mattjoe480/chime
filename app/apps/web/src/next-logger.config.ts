55e4e1e0f6eeae835674f0b3007066420b6de6b7
matt <matthew.jon480@gmail.com>
2025-02-21 14:42:12 +0530

    Create next-logger.config.ts

---
+++ app/apps/web/src/next-logger.config.ts
@@ -0,0 +1,19 @@
+import pino, {Logger} from "pino";
+
+export const logger: Logger =
+        process.env["NODE_ENV"] === "production"
+                ?
+                pino({
+                  level: "warn",
+                  browser: {asObject: true},
+                })
+                :
+                pino({
+                  browser: {asObject: true},
+                  transport: {
+                    target: "pino-pretty",
+                    options: {
+                      colorize: true,
+                    },
+                  },
+                });
